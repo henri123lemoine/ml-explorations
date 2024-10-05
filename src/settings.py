4c8a5eaca6cf7055e507f2587906a53e91e5f220
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-05 13:00:26 -0400

    Added settings.py, with openai and anthropic-specific clients

---
+++ src/settings.py
@@ -0,0 +1,21 @@
+import os
+from datetime import datetime
+
+import anthropic
+import ell
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATE = datetime.now().strftime("%Y-%m-%d")
+
+# Clients
+
+## OpenAI
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+OPENAI_CLIENT = openai.Client(api_key=OPENAI_API_KEY)
+
+## Anthropic
+ANTHROPIC_API_KEY = os.environ.get("ANTHROPIC_API_KEY")
+ANTHROPIC_CLIENT = anthropic.Client(api_key=ANTHROPIC_API_KEY)
