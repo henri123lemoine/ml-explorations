5ad1856264823ff40af943835b04b9157c29d800
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-16 17:06:53 -0400

    Implemented streaming for qwen

--- src/utils/qwen.py
+++ src/utils/qwen.py
@@ -1,4 +1,6 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from threading import Thread
+
+from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 
 model_name = "Qwen/Qwen2-0.5B-Instruct"
 
@@ -16,11 +18,15 @@ messages = [
 text = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
 model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-generated_ids = model.generate(**model_inputs, max_new_tokens=512)
-generated_ids = [
-    output_ids[len(input_ids) :]
-    for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
+streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=512)
 
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+thread = Thread(target=model.generate, kwargs=generation_kwargs)
+thread.start()
+
+print("Streaming output:")
+for text in streamer:
+    print(text, end="", flush=True)
+
+thread.join()
+print("\nStreaming finished.")
