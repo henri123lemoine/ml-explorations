ae6d09c477ba1848877c31fd51bb13bef2fcf850
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-17 18:58:02 -0400

    Renamed hpmor-related files and added todos

---
+++ src/experiments/finetuning/hpmor/main.py
@@ -0,0 +1,10 @@
+# This experiment is about finetuning a Qwen model on HPMOR+
+
+"""
+TODO:
+- Dataset extraction
+    [x] HPMOR (main priority)
+    [ ] HPMOR Fanfics (low priority) -> see https://www.lesswrong.com/posts/uWM5auewdKjdJJG9t/the-book-of-hpmor-fanfics
+- Model finetuning
+    [ ] TODO (compare finetuning methods)
+"""
