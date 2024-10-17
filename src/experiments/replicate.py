a2896712c90914d663b36e77b8672ff735f21f21
Henri Lemoine <henri123lemoine@gmail.com>
2024-10-17 03:06:31 -0400

    nit

--- src/experiments/replicate.py
+++ src/experiments/replicate.py
@@ -1,10 +1,16 @@
 from src.settings import replicate_client
 
-prompt = "Hello"
 
-output = replicate_client.run(
-    "meta/llama-2-70b-chat",
-    input={"prompt": prompt},
-)
-for item in output:
-    print(item, end="")
+def main():
+    prompt = "Hello"
+
+    output = replicate_client.run(
+        "meta/llama-2-70b-chat",
+        input={"prompt": prompt},
+    )
+    for item in output:
+        print(item, end="")
+
+
+if __name__ == "__main__":
+    main()
