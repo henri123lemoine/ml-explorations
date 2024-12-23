58432e779f11b9d439668ac224273e0654ee38d3
Henri Lemoine <henri123lemoine@gmail.com>
2024-12-23 03:32:42 -0500

    moved mnist dataset installation to data/dataset/

--- src/experiments/ensemble_experiment.py
+++ src/experiments/ensemble_experiment.py
@@ -12,6 +12,7 @@ from torchvision import datasets, transforms
 
 from src.models.base import ModelInterface, TorchModel
 from src.models.ensemble import Ensemble, EnsembleMethod, evaluate_ensemble
+from src.settings import DATASETS_PATH
 
 
 def train_model(
@@ -192,8 +193,8 @@ def get_data() -> tuple[DataLoader, DataLoader, DataLoader]:
         [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
     )
 
-    train_dataset = datasets.MNIST("data", train=True, download=True, transform=transform)
-    test_dataset = datasets.MNIST("data", train=False, transform=transform)
+    train_dataset = datasets.MNIST(DATASETS_PATH, train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST(DATASETS_PATH, train=False, transform=transform)
 
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=1000)
